3049c6261e51ac3e64289902baff86524965d305
Paula.Kmicinska <paula.kmicinska@vlaanderen.be>
2021-12-30 21:46:20 +0100

    Initial Commit

---
+++ TravelTrivia/TravelTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTriviaApp.swift
+//  TravelTrivia
+//
+//  Created by PAULA on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TravelTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
